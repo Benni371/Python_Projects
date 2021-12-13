fe438f3ae6a2ac810df498686c1f86b5320d0e5f
Benni371 <bennion.koy@gmail.com>
2021-12-12 19:54:31 -0700

    Done?

--- Projects/PyGui/window.py
+++ Projects/PyGui/window.py
@@ -9,7 +9,7 @@ master.title("Whatever you Want")
 master.geometry('1080x1920')
 width = 1080
 height = 1920
-# color pallete
+# color pallette
 blue = "#0F2C67"
 red = "#CD1818"
 yellow = "#F3950D"
@@ -17,16 +17,17 @@ lightTan= "#F4E185"
 
 
 
-
+# position of the inner background for the labels
 innerBgX= 0.5
 innerBgY= 0.2
 innerBgHeight = 22
 innerBgWidth = 60
+# fonts for the labels
 titleFont = ("Helvetica",20, "bold")
 bodyFont = ("Impact", 20)
 
 
-
+#create four different canvases to display separate data on
 canvas1 = Canvas(master, width=width/2, height=height/2, relief='raised')
 canvas2 = Canvas(master, width=width/2, height=height/2, relief='raised')
 canvas3 = Canvas(master, width=width/2, height=height/2, relief='raised')
@@ -47,19 +48,15 @@ news = hack.get_news()
 for i in range(3):
     master.rowconfigure(i, weight=1)
     master.columnconfigure(i, weight=1)
-
+#set the image for the background
 bgImage = ImageTk.PhotoImage(file="./images/webback.jpg")
+# make a image object for the qr code and resize it
 qr = PhotoImage(file="./images/article.png", )
 qr = qr.zoom(22)
 qr = qr.subsample(40)
 
 
-# Define 4 seperate canvases
-
-
-
-# define which columns the canvas is in
-
+#create backgrounds for the images
 canvas1.create_image(250, 250, image = bgImage)
 canvas2.create_image(250, 250, image = bgImage)
 canvas3.create_image(250, 250, image = bgImage)
@@ -94,8 +91,10 @@ bgLabel4 = Label(canvas4,bg="green",width=innerBgWidth, height=innerBgHeight).pl
 title4   = Label(canvas4, text=dad.name, font=titleFont,bg="white", fg="black").place(relx=0.5, rely=0.02, anchor='n') 
 desc4    = Label(canvas4, text=f"{dadJoke}" , font=bodyFont, foreground="black", bg=aqiStats['Bg'],wraplength=300).place(relx=0.5, rely=0.3, anchor='n') 
 
+# places columns in their respective positions
 canvas1.grid(column=0, row=0, sticky='nwse')
 canvas2.grid(column=1, row=0, sticky='nwse')
 canvas3.grid(column=0, row=1, sticky='nwse')
 canvas4.grid(column=1, row=1, sticky='nwse')
+
 master.mainloop()
